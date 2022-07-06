f5cd2df435fa042badd76dc7088a69153b45730e
Renaldo Abreu <renaldoc@ciandt.com>
2022-07-06 13:35:58 -0300

    Padrao de Projetos Factory

--- 0x00/personagem/Personagem.java
+++ 0x00/personagem/Personagem.java
@@ -1,8 +1,6 @@
 
 
-import lombok.Data;
 
-@Data
 public abstract class Personagem {
 
     private String nome;
@@ -16,6 +14,62 @@ public abstract class Personagem {
         this.forca = forca;
         this.vigor = vigor;
         this.resistencia = resistencia;
+        this.destreza = destreza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoPersonagem getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
         this.destreza = destreza;
     }
 
